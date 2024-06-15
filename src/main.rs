67ac228e82e2aa1226334f3ebc2939e029599863
Tess Ellenoir Duursma <tess@vanu.dev>
2024-06-15 20:26:23 +0200

    Revamp the paramter validation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
 
 fn main() -> wry::Result<()> {
-    let url = env::args()
-        .nth(1)
-        .expect("The app requires a URL parameter!");
-    println!("Opening {}", url);
-    bornscreen::spawn_browser(url)
+    if let Some(url) = env::args().nth(1) {
+        println!("Opening {}", url);
+        bornscreen::spawn_browser(url)
+    } else {
+        println!("BornScreen requires a URL as a parameter.");
+        Ok(())
+    }
 }
