9b4a941109fa4c5901e91ce11bc0abdcf690d513
Tess Ellenoir Duursma <tess@vanu.dev>
2024-06-27 14:50:40 +0200

    Rename the crate to fossbeamer

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 fn main() -> wry::Result<()> {
     if let Some(url) = env::args().nth(1) {
         println!("Opening {}", url);
-        bornscreen::spawn_browser(url)
+        fossbeamer::spawn_browser(url)
     } else {
         println!("BornScreen requires a URL as a parameter.");
         Ok(())
