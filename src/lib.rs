31ffc19c16b92b68268aba4c78166e81f70a8df7
Tess Ellenoir Duursma <tess@vanu.dev>
2024-07-01 22:10:22 +0200

    Add support for commands to the event loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,36 @@
+use std::{sync::mpsc::Receiver, thread};
+
 use tao::{
     event::{Event, StartCause, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
+    event_loop::{ControlFlow, EventLoopBuilder},
     window::WindowBuilder,
 };
 use wry::WebViewBuilder;
 
-pub fn spawn_browser(url: String) -> wry::Result<()> {
-    let event_loop = EventLoop::new();
+#[derive(Debug)]
+pub enum Command {
+    LoadUrl(String),
+    Reload,
+    Stop,
+}
+
+pub fn spawn_browser(url: String, command_receiver: Option<Receiver<Command>>) -> wry::Result<()> {
+    let event_loop = EventLoopBuilder::<Command>::with_user_event().build();
     let window = WindowBuilder::new()
         .with_title("BornScreen")
         .build(&event_loop)
         .unwrap();
-    let _webview = WebViewBuilder::new(&window).with_url(url).build()?;
+    let webview = WebViewBuilder::new(&window).with_url(url).build()?;
+
+    if let Some(command_receiver) = command_receiver {
+        let proxy = event_loop.create_proxy();
+        thread::spawn(move || {
+            while let Ok(command) = command_receiver.recv() {
+                // TODO: Remove the use of unwrap
+                proxy.send_event(command).unwrap();
+            }
+        });
+    }
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
@@ -21,7 +40,16 @@ pub fn spawn_browser(url: String) -> wry::Result<()> {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
-            } => *control_flow = ControlFlow::Exit,
+            }
+            | Event::UserEvent(Command::Stop) => *control_flow = ControlFlow::Exit,
+            Event::UserEvent(Command::Reload) => {
+                if let Ok(url) = webview.url() {
+                    // TODO: Remove the use of unwrap
+                    webview.load_url(url.as_str()).unwrap();
+                }
+            }
+            // TODO: Remove the use of unwrap
+            Event::UserEvent(Command::LoadUrl(url)) => webview.load_url(url.as_str()).unwrap(),
             _ => (),
         }
     })
