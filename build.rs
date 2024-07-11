cfb0d330441713a1dcfb6330d598606fb95cd121
Florian Klink <flokli@flokli.de>
2024-07-11 12:04:41 +0300

    build.rs: make system_deps conditional on outside nix-build
    This is only really a hack to get impure `cargo build` to work,
    so we can just skip using system_deps for this root crate if we detect
    we're running inside a nix build.

--- build.rs
+++ build.rs
@@ -1,3 +1,16 @@
 fn main() {
+    // HACK: steer the impure nix build to zlib via pkg-config,
+    // but don't do that for the pure one.
+    // NOTE: this is a result of us adding a dependency to zlib through
+    // `system_deps`, to get an impure `cargo build` to pick up zlib runpath.
+    // Once rustc stops linking to zlib explicitly, or also does do RUNPATH
+    // propagation, this could be removed.
+    // See https://github.com/NixOS/nixpkgs/pull/325876
+    for (k, v) in std::env::vars() {
+        if &k == "NIX_BUILD_TOP" && v == "/build" {
+            return;
+        }
+    }
+
     system_deps::Config::new().probe().unwrap();
 }
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -2,21 +2,8 @@ self: pkgs: rec {
   fossbeamer_crates = import ../Cargo.nix {
     inherit pkgs;
     nixpkgs = pkgs.path;
-
-    defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-      fossbeamer = prev: {
-        # NOTE: this is a result of us adding a dependency to zlib through
-        # `system_deps`, to get an impure `cargo build` to pick up zlib runpath.
-        # Once rustc stops linking to zlib explicitly, or also does do RUNPATH
-        # propagation, this could be removed.
-        # See https://github.com/NixOS/nixpkgs/pull/325876
-        nativeBuildInputs = [ pkgs.pkg-config ];
-        buildInputs = prev.buildInputs or [ ] ++ [
-          pkgs.zlib
-        ];
-      };
-    };
   };
+
   fossbeamer = fossbeamer_crates.rootCrate.build.override {
     runTests = true;
   };
