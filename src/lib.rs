757bc9d065f837e81c22773477747d45d18ab421
Florian Klink <flokli@flokli.de>
2024-07-18 15:37:33 +0200

    feat: add state/mode structs
    We want nodes to be able to report their state back.
    Use the same types previously introduced in
    https://github.com/flokli/display-agent

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,44 @@
+use serde::{Deserialize, Serialize};
+
 mod browser;
 pub use browser::spawn as spawn_browser;
 pub use browser::Command;
+use tracing::debug;
+
+/// Represents the current configuration for this display.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct State {
+    pub enabled: bool,
+    pub mode: Mode,
+    pub power: bool,
+    pub scale: f64,
+    pub transform: String,
+    pub scenario: Scenario,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Mode {
+    pub width: u64,
+    pub height: u64,
+    pub refresh: f64,
+    pub picture_aspect_ratio: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(tag = "name", content = "args", rename_all = "lowercase")]
+pub enum Scenario {
+    URL { url: String },
+    Blank,
+    Video { url: String },
+}
+
+// Contains more general information about the display
+// (make, model, serial, supported modes)
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Info {
+    pub make: String,
+    pub model: String,
+    pub modes: Vec<Mode>,
+    pub name: String,
+    pub serial: String,
+}
