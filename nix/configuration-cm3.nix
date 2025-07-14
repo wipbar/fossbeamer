5cd0aef842318fa2b4338381da69857e76ac0359
Florian Klink <flokli@flokli.de>
2025-07-14 22:26:23 +0200

    niv: bump nixpkgs
    drop dt overlay, the fix landed in the kernel we're now using.

--- nix/configuration-cm3.nix
+++ nix/configuration-cm3.nix
@@ -1,9 +1,7 @@
 # Machine config, specifically for raspberry cm3
-{ config
-, modulesPath
-, pkgs
-, lib
-, ...
+{
+  pkgs,
+  ...
 }:
 
 let
@@ -20,30 +18,13 @@ in
   sdImage.populateFirmwareCommands = ''
     rm firmware/u-boot-rpi3.bin
     # Overwrite firmware/u-boot-rpi3.bin with the generic one
-    cp ${ubootRaspberryPiGeneric}/u-boot.bin firmware/u-boot-rpi3.bin    
+    cp ${ubootRaspberryPiGeneric}/u-boot.bin firmware/u-boot-rpi3.bin
     # Add the .dtb for our board
-    cp ${pkgs.raspberrypifw}/share/raspberrypi/boot/bcm2710-rpi-cm3.dtb firmware/
+    rm firmware/bcm2710-rpi-cm3.dtb
+    cp ${pkgs.raspberrypifw}/share/raspberrypi/boot/bcm2710-rpi-cm3.dtb firmware/bcm2710-rpi-cm3.dtb
   '';
 
   # u-boot doesn't know the proper device tree name, so cannot pick from FTDIR
   # on its own.
   hardware.deviceTree.name = "broadcom/bcm2837-rpi-cm3-io3.dtb";
-  hardware.deviceTree.overlays = [
-    {
-      # http://lists.infradead.org/pipermail/linux-arm-kernel/2024-July/943386.html
-      name = "fix-hdmi-hpd-gpio-port";
-      dtsText = ''
-        /dts-v1/;
-        /plugin/;
-        / {
-          compatible = "raspberrypi,3-compute-module", "brcm,bcm2837";
-        };
-        &{/soc} {
-          hdmi {
-            hpd-gpios = <&expgpio 0 1>;
-          };
-        };
-      '';
-    }
-  ];
 }
